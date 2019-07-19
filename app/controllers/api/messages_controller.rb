cee86cd789b63cc9df9efd5384e2c6248070d686
本岡 <tennteru21@gmail.com>
2019-07-19 19:31:33 +0900

    indexアクションの作成
    groupと紐づけて値を取得する

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,4 +1,11 @@
 class Api::MessagesController < ApplicationController
-  def index
+  before_action :set_group
+  
+  def index   
+    @messages = @group.messages.where('id > ?', params[:id])
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
