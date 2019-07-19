84e56141e202d43e9bbdc49c0a764ca98f98a740
本岡 <tennteru21@gmail.com>
2019-07-19 10:02:52 +0900

    message.js更新　自動更新実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
 
   function message_build(message){
     var imagehtml = message.image == null ? "" : `<img src="${message.image}" class="lower-message__image">`
-    var html = `<div class="message" >
+    var html = `<div class="message" data-id=${message.id}>
                   <div class = "message__upper-info">
                     <div class = "message__upper-info__talker">
                     ${message.user_name}
@@ -47,4 +47,30 @@ $(function(){
       alert("メッセージの投稿に失敗しました");
     })
   });
+
+    var reloadMessages = function() {
+      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+      var last_message_id = $('.message').last().data('message-id');
+      var group_id = $('.chat').data('group-id')
+      $.ajax({
+        //ルーティングで設定した通りのURLを指定
+        url: '/groups/'+ group_id +'/api/messages',
+        //ルーティングで設定した通りhttpメソッドをgetに指定
+        type: 'get',
+        dataType: 'json',
+        //dataオプションでリクエストに値を含める
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {        
+        messages.forEach(function(message){
+          var insertHTML = message_build(message);
+          $(".messages").append(insertHTML);
+          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+        });        
+      })
+      .fail(function() {
+        alert('自動更新に失敗');
+      });
+    };    
+  setInterval(reloadMessages, 5000);
 });
